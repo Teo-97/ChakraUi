5ac014cc6203afcca70b86dc15ae5a7847fd3665
Mateo L <mateolopezw@hotmail.com>
2023-07-14 11:09:32 -0500

    La magia del chakra

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { Box, Heading, Button, Flex, Stack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Box
+        bg="gray"
+        bgImage="url(https://pspro.ir/mag/image/catalog/Mag/jwst/JWST-carina-nebula.jpg)"
+        bgSize="cover"
+        h="40vh"
+      >
+        <Flex
+          direction="column"
+          alignItems="center"
+          justify="center"
+          h="100%"
+          bg="rgb (0 0 0 / 50%)"
+          p={["0 10%", null, "0 20%"]}
+        >
+          <Heading
+            color="white"
+            textAlign="center"
+            fontWeight="ligth"
+            letterSpacing="5px"
+          >
+            DOMINA EL TERRENO
+          </Heading>
+          <Stack
+            direction={{
+              base: "column",
+              md: "row",
+            }}
+            mt="20px"
+            w={["100%", null, "auto"]}
+          >
+            <Button
+              variant="outline"
+              size="lg"
+              fontWeight="light"
+              borderRadius="0"
+              color="white"
+              _hover={{
+                color: "black",
+                bg: "white",
+              }}
+            >
+              VER DETALLES
+            </Button>
+            <Button
+              variant="outline"
+              size="lg"
+              fontWeight="light"
+              borderRadius="0"
+              color="white"
+              _hover={{
+                color: "white",
+                bg: "black",
+              }}
+            >
+              VER VIDEO
+            </Button>
+          </Stack>
+        </Flex>
+      </Box>
+    </>
+  );
+}
+
+export default App;
